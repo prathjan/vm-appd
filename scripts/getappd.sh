c4d7d3e3d021209844ce301c6e16221c2a872e2b
prathjan <prathjan@cisco.com>
2021-07-09 12:44:36 -0500

    updates4

--- scripts/getappd.sh
+++ scripts/getappd.sh
@@ -28,7 +28,7 @@ function parse_input() {
 
 parse_input
 
-#content=$(curl -s --location --request POST 'https://devnet.saas.appdynamics.com/auth/v1/oauth/token' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=client_credentials' --data-urlencode 'client_id=devclient@devnet' --data-urlencode 'client_secret=${CL_SECRT}' | jq '.access_token') 
+content=$(curl -s --location --request POST 'https://devnet.saas.appdynamics.com/auth/v1/oauth/token' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=client_credentials' --data-urlencode 'client_id=devclient@devnet' --data-urlencode 'client_secret=${CL_SECRT}' | jq '.access_token') 
 
 #content=$(curl -s --location --request POST 'https://devnet.saas.appdynamics.com/auth/v1/oauth/token' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=client_credentials' --data-urlencode 'client_id=devclient@devnet' --data-urlencode 'client_secret=52961b11-dd9d-42ee-8426-917f4adb8e75' | jq '.access_token') 
 
