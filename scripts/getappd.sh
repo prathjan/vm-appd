92aecad6780f7d40fd09054962d096dcd2799db0
prathjan <prathjan@cisco.com>
2021-07-09 13:23:22 -0500

    updates5

--- scripts/getappd.sh
+++ scripts/getappd.sh
@@ -32,14 +32,14 @@ content=$(curl -s --location --request POST 'https://devnet.saas.appdynamics.com
 
 #content=$(curl -s --location --request POST 'https://devnet.saas.appdynamics.com/auth/v1/oauth/token' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=client_credentials' --data-urlencode 'client_id=devclient@devnet' --data-urlencode 'client_secret=52961b11-dd9d-42ee-8426-917f4adb8e75' | jq '.access_token') 
 
-#temp="${content%\"}"
-#temp="${temp#\"}"
+temp="${content%\"}"
+temp="${temp#\"}"
 #echo "$temp"
 
 #download="$(curl -s --location --request GET 'https://devnet.saas.appdynamics.com/zero/v1beta/install/downloadCommand?javaVersion=latest&machineVersion=latest&infraVersion=latest&zeroVersion=latest&multiline=false' \
 
-#download="$(curl -s --location --request GET "https://devnet.saas.appdynamics.com/zero/v1beta/install/downloadCommand?javaVersion=${JVER}&machineVersion=latest&infraVersion=latest&zeroVersion=latest&multiline=false" \
-#--header "Authorization: Bearer ${temp}" --data-raw '')"
+download="$(curl -s --location --request GET "https://devnet.saas.appdynamics.com/zero/v1beta/install/downloadCommand?javaVersion=${JVER}&machineVersion=latest&infraVersion=latest&zeroVersion=latest&multiline=false" \
+--header "Authorization: Bearer ${temp}" --data-raw '')"
 
 #install="$(curl -s --location --request GET "https://devnet.saas.appdynamics.com/zero/v1beta/install/installCommand?sudo=true&multiline=false&application="yyy"&accessKey="yyy"&serviceUrl=https://devnet.saas.appdynamics.com" \
 #--header "Authorization: Bearer ${temp}" --data-raw '')"
